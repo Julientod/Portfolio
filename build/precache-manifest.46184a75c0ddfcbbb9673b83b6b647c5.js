1b494282c2dea83bda24ebda1e2c30361f9deb1d
Julientod <julien.todouawoga@gmail.com>
2024-10-03 15:23:46 +0200

    deployement fixed

---
+++ build/precache-manifest.46184a75c0ddfcbbb9673b83b6b647c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c50e12470369b1077185",
+    "url": "/static/css/main.150587a4.chunk.css"
+  },
+  {
+    "revision": "c50e12470369b1077185",
+    "url": "/static/js/main.c50e1247.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bcea258604f616dc23158faaac5caede",
+    "url": "/index.html"
+  }
+];
