6f22ca7c824a9076ee7ffebc92d88ad3abcf06aa
Julientod <julien.todouawoga@gmail.com>
2024-10-03 15:06:55 +0200

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,82 @@
+let resumeData = {
+    "name": "TODOUAWOGA Julien",
+    "role": "Frontend Developer ",
+    "linkedinId": "Julien Tod",
+    "roleDescription": "Je suis un développeur passionné, avec un intérêt particulier pour l'exploration des diverses facettes du développement web. Toujours en quête d'amélioration, j'aime me tenir à jour sur les nouvelles technologies. Dans mon temps libre, je m'engage dans des projets personnels pour affiner mes compétences et élargir mon expertise.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/julien-tod-804000328/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Julientod",
+          "className":"fa fa-github"
+        },
+      ],
+
+    "aboutme":"J'ai suivi la formation d'intégrateur web de l'organisme Openclassrooms pour apprendre les bases du métier de développeur web. Je compte par la suite continuer à me former en suivant une formation sur le reactjs pour affiner mes compétences.",
+    "address":"France",
+    "email":"julien.todouawoga@gmail.com",
+    "education":[
+      {
+        "Formation":"Intégrateur Web",
+        "MonthOfPassing":"sept",
+        "YearOfPassing":"2024",
+        
+      },
+      
+    ],
+    "work":[
+      {
+        "CompanyName":"",
+        "specialization":"",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"",
+      },
+      {
+        "CompanyName":"",
+        "specialization":"",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"",
+        
+      }
+    ],
+    "skillsDescription":"",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Kasa",
+        "description":"",
+        "imgurl":"images/logo_kaza.png",
+        "url":"https://github.com/Julientod/Projet-Kaza"
+      },
+      {
+        "name":"OhMyFood",
+        "description":"",
+        "imgurl":"images/ohmyfood.png",
+        "url":"https://github.com/Julientod/Ohmyfood"
+      },
+
+      {
+        "name":"Sophie-Bluel",
+        "description":"",
+        "imgurl":"images/sophie-bluel.png",
+        "url":"https://github.com/Julientod/Projet-6"
+      },
+      
+    ],
+  }
+  
+  export default resumeData
