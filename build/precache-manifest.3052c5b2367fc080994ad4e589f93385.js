42fa8332b49a35a914fc44ab191ff2fbbadbc1b5
Julientod <julien.todouawoga@gmail.com>
2024-10-15 15:22:56 +0200

    json updtae

---
+++ build/precache-manifest.3052c5b2367fc080994ad4e589f93385.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "77b041652197db5b5f15",
+    "url": "/Portfoliogi/static/css/main.150587a4.chunk.css"
+  },
+  {
+    "revision": "77b041652197db5b5f15",
+    "url": "/Portfoliogi/static/js/main.77b04165.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/Portfoliogi/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "7482ab1e4c9e3e9fe159",
+    "url": "/Portfoliogi/static/js/runtime~main.7482ab1e.js"
+  },
+  {
+    "revision": "85f2ebb6d2c97d9ab2fd9f069c624582",
+    "url": "/Portfoliogi/index.html"
+  }
+];
